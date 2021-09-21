68232f8fb62d6ad8ccbda03be76e2a6f70d30899
vladmir.pavluchinskiy <vladmir.pavluchinskiy@gmail.com>
2021-09-21 22:14:28 +0300

    lab1-5

--- lab1-5/src/main/java/Main.java
+++ lab1-5/src/main/java/Main.java
@@ -1,8 +1,25 @@
 public class Main {
-    public static void main(String[] args) {
+    static volatile boolean isPrinted = false;
+
+    public static void main(String[] args) throws InterruptedException {
+        //async
         CharacterPrinter characterPrinter1 = new CharacterPrinter('-');
         CharacterPrinter characterPrinter2 = new CharacterPrinter('|');
         characterPrinter1.start();
         characterPrinter2.start();
+
+        characterPrinter1.join();
+        characterPrinter2.join();
+
+        System.out.println("00000000000000000000000000000000000000000000000000000000");
+        //sync
+        String lock = "lock";
+
+        SyncCharacterPrinter syncCharacterPrinter1 = new SyncCharacterPrinter('-', lock);
+        syncCharacterPrinter1.setName("-");
+        SyncCharacterPrinter syncCharacterPrinter2 = new SyncCharacterPrinter('|', lock);
+        syncCharacterPrinter2.setName("|");
+        syncCharacterPrinter1.start();
+        syncCharacterPrinter2.start();
     }
 }
--- lab1-5/src/main/java/SyncCharacterPrinter.java
+++ lab1-5/src/main/java/SyncCharacterPrinter.java
@@ -1,2 +1,46 @@
 public class SyncCharacterPrinter extends Thread {
+    private final char symbol;
+    private final String lock;
+
+
+    public SyncCharacterPrinter(char symbol, String lock) {
+        this.symbol = symbol;
+        this.lock = lock;
+
+    }
+
+    @Override
+    public void run() {
+        for (int j = 0; j < 50; j++) {
+            for (int i = 0; i < 20; i++) {
+                synchronized (lock) {
+                    if (Main.isPrinted && symbol == '|') {
+                        try {
+                            lock.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    if (!Main.isPrinted && symbol == '-') {
+                        try {
+                            lock.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    System.out.print(symbol);
+
+                    Main.isPrinted = !Main.isPrinted;
+
+                    lock.notify();
+
+                }
+
+            }
+            System.out.println();
+        }
+    }
 }
+
